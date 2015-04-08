af2bcc848a5da8a106028b923f729b6bebe32f97
KovalKt <katrysasonce@gmail.com>
2015-04-08 11:22:50 +0300

    Added base link drawing functionality

--- Objects.py
+++ Objects.py
@@ -1,4 +1,6 @@
-
+from PyQt4.QtGui import QColor
+from PyQt4.QtGui import QFont
+from PyQt4.QtGui import QPen
 CIRCLE_SIZE = 36
 CIRCLE_R = CIRCLE_SIZE/2
 
@@ -10,6 +12,10 @@ class Node():
         self.center_x = center_x
         self.center_y = center_y
         self.weight = 0
+        self.selected = False
+
+    def __repr__(self):
+        return 'Node %s' % self.id
 
     def is_selected(self, x1, y1):
         return (x1 < (self.center_x + CIRCLE_R) and x1 > (self.center_x - CIRCLE_R) and 
@@ -23,3 +29,43 @@ class Node():
         self.center_y = new_y
         self.x = new_x - CIRCLE_R
         self.y = new_y - CIRCLE_R
+
+    def draw_itself(self, event, qp):
+        circle_color = QColor(204, 0, 0) if self.selected else QColor(0, 0, 0)
+        qp.setPen(QPen(circle_color, 3))
+        qp.setBrush(QColor(255, 255, 255))
+        qp.drawEllipse(self.x, self.y, CIRCLE_SIZE, CIRCLE_SIZE)
+        # draw node id
+        text = str(self.id)
+        qp.setPen(QColor(102, 0, 0))
+        qp.setFont(QFont('Decorative', 10))
+        qp.drawText(self.x, self.y, text)  
+
+        # draw node weight
+        text = str(self.weight)
+        qp.setPen(QColor(25, 0, 51))
+        qp.setFont(QFont('Decorative', 10))
+        qp.drawText(self.center_x-5, self.center_y+5, text)
+
+class Line():
+    def __init__(self, id, from_node, to_node):
+        self.id = id
+        self.from_node = from_node
+        self.to_node = to_node
+        # self.center_x = center_x
+        # self.center_y = center_y
+        self.weight = 0
+        self.selected = False
+
+    def is_selected(self, x, y):
+        pass
+
+    def draw_itself(self, event, qp):
+        qp.setPen(QPen(QColor(0, 0, 0, 150), 2, 2))
+        qp.drawLine(
+            self.from_node.center_x, 
+            self.from_node.center_y, 
+            self.to_node.center_x, 
+            self.to_node.center_y,
+        )
+
--- main_execute.py
+++ main_execute.py
@@ -7,7 +7,8 @@ from PyQt4 import QtGui, QtCore
 
 from Objects import *
 
-index_gen = count()
+node_index_gen = count()
+line_index_gen = count()
 
 
 class MainGui(QtGui.QMainWindow):
@@ -15,6 +16,7 @@ class MainGui(QtGui.QMainWindow):
     def __init__(self):
         super(MainGui, self).__init__()
         self.node_list = []
+        self.line_list = []
         self.node_posicion_map = dict()
         self.selected_node = None
         self.initUI()
@@ -46,15 +48,28 @@ class MainGui(QtGui.QMainWindow):
     def mousePressEvent(self, event):
         x = event.pos().x()  # - CIRCLE_SIZE/2
         y = event.pos().y()  # - CIRCLE_SIZE/2
-        self.selected_node = find_selected_node(self.node_list, x, y)
-        if not self.selected_node:
-            node_weight_dialog = DialogWindow()
-            weight, ans = node_weight_dialog.showDialog()
-            if ans:
-                new_node = Node(index_gen.next(), x, y)
-                new_node.set_weight(weight)
-                self.node_list.append(new_node)
-                self.node_posicion_map[(x, y)] = new_node
+        selected_node2 = find_selected_node(self.node_list, x, y)
+        # print 'on mousePressEvent', self.selected_node, selected_node2
+        if self.selected_node:
+            if selected_node2:
+                # draw line
+                new_line = Line(line_index_gen.next(), self.selected_node, selected_node2)
+                self.line_list.append(new_line)
+            self.selected_node.selected = False
+            self.selected_node = None 
+        else:            
+            if selected_node2:
+                self.selected_node = selected_node2
+                selected_node2.selected = True
+            else:    
+                node_weight_dialog = DialogWindow()
+                weight, ans = node_weight_dialog.showDialog('Enter node weight:')
+                if ans:
+                    new_node = Node(node_index_gen.next(), x, y)
+                    new_node.set_weight(weight)
+                    self.node_list.append(new_node)
+                    self.node_posicion_map[(x, y)] = new_node
+
 
 
     def mouseMoveEvent(self, event):
@@ -62,6 +77,15 @@ class MainGui(QtGui.QMainWindow):
         new_y = event.pos().y()
         if self.selected_node:
             self.selected_node.set_new_coordinate(new_x, new_y)
+
+    def keyPressEvent(self, event):
+        if event.key() == QtCore.Qt.Key_E:
+            if self.selected_node:
+                node_weight_dialog = DialogWindow()
+                weight, ans = node_weight_dialog.showDialog('Enter new node weight:')
+                if ans:
+                    self.selected_node.set_weight(weight)
+
     
     def drawPoints(self, qp):
         pen = QtGui.QPen(QtCore.Qt.blue, 3)
@@ -84,24 +108,14 @@ class MainGui(QtGui.QMainWindow):
         pen = QtGui.QPen(QtCore.Qt.black, 3)
         qp.setPen(pen)
 
+        for line in self.line_list:
+            line.draw_itself(event, qp)
         for node in self.node_list:
-            qp.setPen(QtGui.QPen(QtCore.Qt.black, 3))
-            qp.drawEllipse(node.x, node.y, CIRCLE_SIZE, CIRCLE_SIZE)
-            self.draw_id(event, qp, node)
-            self.draw_weight(event, qp, node)
-
-
-    def draw_id(self, event, qp, obj):
-        text = str(obj.id)
-        qp.setPen(QtGui.QColor(255, 80, 0))
-        qp.setFont(QtGui.QFont('Decorative', 10))
-        qp.drawText(obj.x, obj.y, text)  
-
-    def draw_weight(self, event, qp, obj):
-        text = str(obj.weight)
-        qp.setPen(QtCore.Qt.blue)
-        qp.setFont(QtGui.QFont('Decorative', 10))
-        qp.drawText(obj.center_x-5, obj.center_y+5, text)  
+            node.draw_itself(event, qp)
+            # qp.drawEllipse(node.x, node.y, CIRCLE_SIZE, CIRCLE_SIZE)
+            # self.draw_id(event, qp, node)
+            # self.draw_weight(event, qp, node)
+  
 
 
 def find_selected_node(node_list, x, y):
@@ -127,10 +141,10 @@ class DialogWindow(QtGui.QWidget):
         self.setGeometry(300, 300, 290, 150)
         self.setWindowTitle('Input dialog')
         
-    def showDialog(self):
+    def showDialog(self, input_label):
         
         text, ok = QtGui.QInputDialog.getText(self, 'Input Dialog', 
-            'Enter node weight:')
+            input_label)
         
         if ok:
             self.le.setText(str(text))
